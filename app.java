b815a3e9b1019ac9ab63c40fc85232e743bd6374
shivadurga6618 <shiva.pilla6618@gmail.com>
2026-07-17 19:01:20 +0530

    Update app.java

--- app.java
+++ app.java
@@ -3,5 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, DevOps Pipeline!");
+        System.out.println("This is my second message.");
+        System.out.println("Java is running successfully.");
     }
 }
